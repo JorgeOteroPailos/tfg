0dbbc522f53ce7aff759f6e765b48f016ccb95bc
Jorge Otero Pailos <154424142+JorgeOteroPailos@users.noreply.github.com>
2026-04-28 10:18:38 +0200

    Handle authentication errors in GlobalExceptionHandler
    Add exception handling for authentication errors.

--- backend/src/main/java/gal/usc/telariabackend/Controllers/GlobalExceptionHandler.java
+++ backend/src/main/java/gal/usc/telariabackend/Controllers/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package gal.usc.telariabackend.Controllers;
 import gal.usc.telariabackend.Model.Exceptions.AlreadyExistingUserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
@@ -15,6 +18,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemDetail error = ProblemDetail.forStatus(HttpStatus.CONFLICT);
         error.setTitle("User already exists");
         error.setType(MvcUriComponentsBuilder.fromController(GlobalExceptionHandler.class).pathSegment("error","user-already-exists").build().toUri());
+        error.setDetail(e.getMessage());
+        return error;
+    }
+
+    @ExceptionHandler({BadCredentialsException.class, InternalAuthenticationServiceException.class})
+    public ProblemDetail handleAuthErrors(Exception e) {
+        ProblemDetail error = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
+        error.setTitle("Authentication failed");
+        error.setType(MvcUriComponentsBuilder.fromController(GlobalExceptionHandler.class)
+                .pathSegment("error", "authentication-failed").build().toUri());
         error.setDetail(e.getMessage());
         return error;
     }
